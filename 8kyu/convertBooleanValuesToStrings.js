736cbbb44c2a824483fbb2efcd2bb2b107b11aca
Garrett Stewart <garrettbstewart@gmail.com>
2023-01-21 21:06:30 -0500

    Refactored, Made Shorter with Ternary

--- 8kyu/convertBooleanValuesToStrings.js
+++ 8kyu/convertBooleanValuesToStrings.js
@@ -1,5 +1,7 @@
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
+// OLD CODE:
+/*
 function boolToWord(bool) {
   //...
   if (bool === true) {
@@ -8,5 +10,8 @@ function boolToWord(bool) {
     return "No";
   }
 }
+*/
 
-// Needs Refactoring. Can be made shorter. Turnary.
+// Needs Refactoring. Can be made shorter. Ternary.
+// NEW CODE:
+const boolToWord = (bool) => (bool ? "Yes" : "No");
