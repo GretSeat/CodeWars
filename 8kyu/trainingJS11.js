1a1c4e03f8f46357ebdbf83c2bb2467774b7ba3a
Garrett Stewart <garrettbstewart@gmail.com>
2023-03-08 11:42:01 -0500

    added continue

--- 8kyu/trainingJS11.js
+++ 8kyu/trainingJS11.js
@@ -44,6 +44,8 @@ function grabDoll(dolls) {
   for (let i = 0; i < dolls.length; i++) {
     if (dolls[i] === "Hello Kitty" || dolls[i] === "Barbie doll") {
       bag.push(dolls[i]);
+    } else {
+      continue;
     }
     if (bag.length === 3) {
       break;
